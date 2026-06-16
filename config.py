70250d3946b36da75e7a600041c4784163fa1375
parthdchavan <parthchavan@example.com>
2026-06-16 14:22:14 +0530

    Blog app

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-SECRET_TOKEN = os.getenv("SECRET_TOKEN")
